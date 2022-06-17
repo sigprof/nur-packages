80b736f9a6190cdeab54817f2784c8e4f074afd8
Sergey Vlasov <sigprof@gmail.com>
2022-06-17 11:28:40 +0300

    draft langpack generation

---
+++ pkgs/mozilla-langpack/packages.nix
@@ -0,0 +1,9 @@
+{ callPackage, recurseIntoAttrs, firefox, firefox-esr, thunderbird }:
+let
+  langpackPackages = args: recurseIntoAttrs (callPackage ./langpack.nix args);
+in
+{
+  firefox-langpack = langpackPackages { mozillaApp = firefox; };
+  firefox-esr-langpack = langpackPackages { mozillaApp = firefox-esr; };
+  thunderbird-langpack = langpackPackages { mozillaApp = thunderbird; };
+}
