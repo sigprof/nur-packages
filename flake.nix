5387f523b5fabc57dca20f1c2ea7fb0fbe5f492a
Sergey Vlasov <sigprof@gmail.com>
2022-06-18 23:45:12 +0300

    Add `pre-commit-hooks` to enforce the formatting
    Use https://github.com/cachix/pre-commit-hooks.nix to run Alejandra
    automatically before every commit (and also during `nix flake check`).

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    pre-commit-hooks,
   }: let
     nurPackageOverlay = import ./overlay.nix;
   in
@@ -26,5 +31,30 @@
           packages = flake-utils.lib.filterPackages system nurPackages;
         }
       )
+    )
+    // (
+      let
+        checkedSystems = with flake-utils.lib.system; [
+          x86_64-linux
+          x86_64-darwin
+          aarch64-linux
+          aarch64-darwin
+          # No `i686-linux` because `pre-commit-hooks` does not evaluate
+        ];
+      in
+        flake-utils.lib.eachSystem checkedSystems (system: {
+          checks = {
+            pre-commit = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                alejandra.enable = true;
+              };
+            };
+          };
+
+          devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+            inherit (self.checks.${system}.pre-commit) shellHook;
+          };
+        })
     );
 }
