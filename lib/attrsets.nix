5f97c75fb477e0a5f999a5648a14bb54037e51e3
Sergey Vlasov <sigprof@gmail.com>
2022-07-09 21:14:34 +0300

    lib: Add `concatMapAttrsToListRecursive`
    The existing `flattenAttrs` function turned out to be not generic enough
    for some use cases; in particular, getting the path to the attribute for
    further processing would be possible only through converting the path to
    a string and back again.  Extract the inner logic of that function into
    a separate function (`concatMapAttrsToListRecursive`), then reimplement
    `flattenAttrs` in terms of that function.

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -1,8 +1,88 @@
 {nixpkgs, ...}: let
   inherit (builtins) attrNames concatMap isAttrs listToAttrs;
   inherit (nixpkgs.lib.attrsets) nameValuePair;
+
+  # concatMapAttrsToListRecursive ::
+  #   ([string] -> any -> [any]|null) -> set|any -> [any]
+  #
+  # Process attribute values of a set recursively in the depth-first order,
+  # then concatenate the results of processing into a flat list.
+  #
+  # Equivalent to `concatMapAttrsToListRecursive' mapper [] rootValue`.
+  #
+  # Arguments:
+  #
+  # - `mapper`: The processing function.  Takes two arguments: `path` (a list
+  #   of strings describing the path to the current value) and `value` (the
+  #   current value).
+  #
+  #   The return value of `mapper` must be one of the following:
+  #   - A list; in this case the returned list will be concatenated into the
+  #     result of `concatMapAttrsToListRecursive`, and the current value will
+  #     not be processed recursively even if it is a set.  Returning an empty
+  #     list is allowed and will result in effectively ignoring the value.
+  #   - `null`; in this case `concatMapAttrsToListRecursive` will process the
+  #     current value recursively if it is a set, or ignore it otherwise.
+  #
+  #   Note that `mapper` will first be called for the `rootValue` itself, and
+  #   must handle that situation correctly (by returning `null` if recursive
+  #   processing is desired).
+  #
+  # - `rootValue`: The set to be processed (although a value of any type
+  #   acceptable for `mapper` could be passed, in which case the function will
+  #   return an empty list if `mapper` returns `null`).
+  #
+  concatMapAttrsToListRecursive = mapper: rootValue:
+    concatMapAttrsToListRecursive' mapper [] rootValue;
+
+  # concatMapAttrsToListRecursive' ::
+  #   ([string] -> any -> [any]|null) -> [string] -> set|any -> [any]
+  #
+  # Process attribute values of a set recursively in the depth-first order,
+  # then concatenate the results of processing into a flat list.
+  #
+  # The difference from `concatMapAttrsToListRecursive` is that this function
+  # has an additional argument to specify the root path instead of starting
+  # from an empty list, so it can be used to process a subtree without wrapping
+  # the processing function to adjust the path.
+  #
+  # Arguments:
+  #
+  # - `mapper`: The processing function.  Takes two arguments: `path` (a list
+  #   of strings describing the path to the current value) and `value` (the
+  #   current value).
+  #
+  #   The return value of `mapper` must be one of the following:
+  #   - A list; in this case the returned list will be concatenated into the
+  #     result of `concatMapAttrsToListRecursive'`, and the current value will
+  #     not be processed recursively even if it is a set.  Returning an empty
+  #     list is allowed and will result in effectively ignoring the value.
+  #   - `null`; in this case `concatMapAttrsToListRecursive'` will process the
+  #     current value recursively if it is a set, or ignore it otherwise.
+  #
+  #   Note that `mapper` will first be called for the `rootValue` itself, and
+  #   must handle that situation correctly (by returning `null` if recursive
+  #   processing is desired).
+  #
+  # - `rootPath`: A list of strings which will be used as the path for
+  #   `rootValue`.
+  #
+  # - `rootValue`: The attribute set to be processed (although a value of any
+  #   type acceptable for `mapper` could be passed, in which case the function
+  #   will return an empty list if `mapper` returns `null`).
+  #
+  concatMapAttrsToListRecursive' = mapper: rootPath: rootValue: let
+    result = mapper rootPath rootValue;
+  in
+    if result != null
+    then result
+    else if isAttrs rootValue
+    then concatMap (n: concatMapAttrsToListRecursive' mapper (rootPath ++ [n]) rootValue.${n}) (attrNames rootValue)
+    else [];
 in {
   attrsets = {
+    inherit concatMapAttrsToListRecursive concatMapAttrsToListRecursive';
+
     # Convert a nested attribute set into a flat one.
     #
     # Parameters:
@@ -35,13 +115,13 @@ in {
     # called for the `nestedAttrs` value itself with an empty list as the path.
     #
     flattenAttrs = valueCond: recurseCond: pathToName: nestedAttrs: let
-      recurse = path: value:
+      process = path: value:
         if valueCond path value
         then [(nameValuePair (pathToName path) value)]
         else if (isAttrs value) && (recurseCond path value)
-        then concatMap (n: recurse (path ++ [n]) value.${n}) (attrNames value)
+        then null
         else [];
     in
-      listToAttrs (recurse [] nestedAttrs);
+      listToAttrs (concatMapAttrsToListRecursive process nestedAttrs);
   };
 }
