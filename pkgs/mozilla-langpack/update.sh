99355ea9259699f36f194cbe1ca2ff4148e79a8e
Sergey Vlasov <sigprof@gmail.com>
2022-07-11 11:44:32 +0300

    Rewrite the update script again
    Now the script accepts arguments like "firefox-91.11.0esr", so the NixOS
    package name could be passed there.
    
        ./pkgs/mozilla-langpack/update.sh $( for channel in "" nixos-21.11 nixos-22.05 nixos-unstable nixpkgs-unstable; do ( [ -n "$channel" ] && export NIX_PATH=nixpkgs=channel:$channel;  nix-instantiate --eval --json '<nixpkgs>' -A firefox.name -A thunderbird.name -A firefox-esr.name; ) done | jq -r )

--- pkgs/mozilla-langpack/update.sh
+++ pkgs/mozilla-langpack/update.sh
@@ -6,6 +6,8 @@ set -eux -o pipefail
 source_dir="${0%/*}"
 
 # Set up a secure temporary directory with automatic cleanup.
+original_umask="$(umask)"
+umask 077
 tmpdir=
 cleanup() {
   [ -n "$tmpdir" ] && rm -rf "$tmpdir" ||:
@@ -14,90 +16,124 @@ trap cleanup EXIT
 tmpdir="$(mktemp -d)"
 
 # Set up a clean environment.
-umask 077
 mkdir "$tmpdir/home"
 export HOME="$tmpdir/home"
 mkdir "$tmpdir/gnupghome"
 export GNUPGHOME="$tmpdir/gnupghome"
 
-# Parse the command line.
-baseName="$1" && shift
-declare -a majorVersions
-majorVersions=("$@")
-
+# Get the signing key to verify the signatures on hash lists.
 gpg --receive-keys 14F26682D0916CDD81E37B6D61B7B526D98F0353
 
-url="https://archive.mozilla.org/pub/${baseName}/releases/"
-
-curl --silent -o "$tmpdir/index.html" "$url"
-allVersions="$(
-  xmllint --html --xpath '//a/text()' "$tmpdir/index.html" | \
+declare -A releaseUrl releaseList
+getReleaseList() {
+  local app="$1" && shift
+  if [ -n "${releaseUrl[$app]:-}" ] && [ -n "${releaseList[$app]:-}" ]; then
+    return 0
+  fi
+  local url="https://archive.mozilla.org/pub/${app}/releases/"
+  local index="$tmpdir/releaseIndex-$app"
+  local list="$tmpdir/releaseList-$app"
+  curl --silent -o "$index" "$url"
+  xmllint --html --xpath '//a/text()' "$index" | \
     sed -e '/^[^0-9]/d' -e '/funnelcake/d' -e 's,/$,,' -e '/b/d' -e '/rc/d' | \
-    sort --version-sort
-)"
+    sort --reverse --version-sort \
+    > "$list"
+  releaseUrl[$app]="$url"
+  releaseList[$app]="$list"
+}
 
-declare -a neededVersions
-for major in "${majorVersions[@]}"; do
-  majorNum="${major%%esr}"
-  if [ "$majorNum" = "$major" ]; then
-    isESR=
-  else
-    isESR=t
-  fi
-  version="$(
-    printf '%s\n' "$allVersions" | \
-      grep -E "^${majorNum}\." | \
-      if [ -n "$isESR" ]; then
-        grep 'esr$'
-      else
-        grep -v 'esr$'
-      fi | \
-      tail -1
-  )"
-  neededVersions+=("$version")
-done
-latestNonESRVersion="$(
-  printf '%s\n' "$allVersions" |
-  grep -v 'esr$' | \
-  tail -1
-)"
-latestESRVersion="$(
-  printf '%s\n' "$allVersions" |
-  grep 'esr$' | \
-  tail -1
-)"
-neededVersions+=("$latestNonESRVersion" "$latestESRVersion")
+getAppLatestVersion() {
+  local app="$1" && shift
+  local majorNumber="$1" && shift
+  local maybeESR="$1" && shift
 
-versionList="$(
-  printf '%s\n' "${neededVersions[@]}" | \
-    sort --version-sort -u
-)"
+  getReleaseList "$app"
+  cat "${releaseList[$app]}" | \
+    if [ -n "$majorNumber" ]; then
+      grep "^${majorNumber}\."
+    else
+      cat
+    fi | \
+    if [ -n "$maybeESR" ]; then
+      grep -m 1 'esr$'
+    else
+      grep -m 1 -v 'esr$'
+    fi
+}
 
-for version in $versionList; do
-  majorVersion="${version%%.*}"
+declare -A appMajorToVersion
+processAppNameWithVersion() {
+  local nameWithVersion="$1" && shift
+  local app="${nameWithVersion%-*}"
+  local version="${nameWithVersion##*-}"
+  local majorNumber="${version%%.*}"
+  local maybeESR=
   if [ -z "${version##*esr}" ]; then
-    majorVersion="${majorVersion}esr"
+    maybeESR=esr
   fi
-  curl --silent -o $HOME/shasums "$url$version/SHA256SUMS"
-  curl --silent -o $HOME/shasums.asc "$url$version/SHA256SUMS.asc"
+  if [ -z "${appMajorToVersion[${app}:${majorNumber}:${maybeESR}]:-}" ]; then
+    getReleaseList "$app"
+    local latestVersion="$( getAppLatestVersion "$app" "$majorNumber" "$maybeESR" )" ||:
+    if [ -n "$latestVersion" ]; then
+      appMajorToVersion[${app}:${majorNumber}:${maybeESR}]="$latestVersion"
+    fi
+  fi
+}
+
+# Parse the command line.
+for nameWithVersion in "$@"; do
+  processAppNameWithVersion "$nameWithVersion"
+done
+
+# Add latest versions for all mentioned apps.
+declare -A allApps
+for appKey in "${!appMajorToVersion[@]}"; do
+  app="${appKey%%:*}"
+  majorNumber="${appKey#*:}"
+  maybeESR="${majorNumber#*:}"
+  majorNumber="${majorNumber%%:*}"
+  maybeESR="${maybeESR%%:*}"
+  allApps[$app:$maybeESR]=1
+done
+for appKey in "${!allApps[@]}"; do
+  app="${appKey%%:*}"
+  maybeESR="${appKey##*:}"
+  latestVersion="$( getAppLatestVersion "$app" "" "$maybeESR" )"
+  processAppNameWithVersion "$app-$latestVersion"
+done
+
+# Process all requested apps and versions.
+for appKey in "${!appMajorToVersion[@]}"; do
+  app="${appKey%%:*}"
+  majorNumber="${appKey#*:}"
+  maybeESR="${majorNumber#*:}"
+  majorNumber="${majorNumber%%:*}"
+  maybeESR="${maybeESR%%:*}"
+  version="${appMajorToVersion[$appKey]}"
+  majorKey="${majorNumber}${maybeESR}"
+  url="${releaseUrl[$app]}"
+
+  curl --silent -o $HOME/shasums "$url$version/SHA512SUMS"
+  curl --silent -o $HOME/shasums.asc "$url$version/SHA512SUMS.asc"
   gpgv --keyring=$GNUPGHOME/pubring.kbx $HOME/shasums.asc $HOME/shasums
 
   for arch in linux-x86_64 linux-i686; do
-    < "$HOME/shasums" \
-      grep -F "${arch}" | \
-      grep '\.xpi$' | \
-      while read -r sha256 filename rest; do
+    cat "$HOME/shasums" | \
+      ( grep -F "${arch}" || true ) | \
+      ( grep '\.xpi$' || true ) | \
+      while read -r sha512 filename rest; do
         this_url="${url}${version}/${filename}"
         this_locale="${filename##*/}"
         this_locale="${this_locale%.xpi}"
         cat >> "$tmpdir/sources.mjson" <<EOF
 {
-  "${baseName}": {
-    "${majorVersion}": {
+  "${app}": {
+    "${majorKey}": {
       "${arch}": {
         "${this_locale}": {
+          "version": "${version}",
           "url": "${this_url}",
-          "sha256": "${sha256}"
+          "sha512": "${sha512}"
         }
       }
     }
@@ -108,6 +144,6 @@ EOF
   done
 done
 
-jq -n 'reduce inputs as $x ({}; . * $x)' < "$tmpdir/sources.mjson" > "$tmpdir/sources.json"
+jq -Sn 'reduce inputs as $x ({}; . * $x)' < "$tmpdir/sources.mjson" > "$tmpdir/sources.json"
 
 mv "$tmpdir/sources.json" "$source_dir/sources.json"
