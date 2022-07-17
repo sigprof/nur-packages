b40e61f040d85aa1feef04e387dea3100ac55936
Sergey Vlasov <sigprof@gmail.com>
2022-07-17 20:36:05 +0300

    Add NixOS module with `ru_RU`-specific settings
    New module options:
    
    - `sigprof.i18n.ru_RU.enable`: Enable the I18N settings appropriate for
      the `ru_RU.UTF-8` locale.  Sets the default system locale, console
      keymap and font, GRUB 2 font, X11 keymap (including some personalized
      value for `services.xserver.xkbOptions).

--- modules/default.nix
+++ modules/default.nix
@@ -8,6 +8,7 @@ in
       hardware/gpu/driver/nvidia/legacy_340.nix
       hardware/printers/driver/hplip.nix
       hardware/sane/backend/epkowa.nix
+      i18n/ru_RU.nix
       nixpkgs/permitted-unfree-packages.nix
     ];
     mergedModuleName = "default";
